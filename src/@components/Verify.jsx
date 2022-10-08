a9e2daaf0600fe46f77a12c837ff9c24b6a7f2db
motuncoded <motunrayoadeneye@example.com>
2022-10-08 23:43:40 +0100

    refactor

--- src/@components/Verify.jsx
+++ src/@components/Verify.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import AuthCode from 'react-auth-code-input';
 import { useNavigate } from 'react-router-dom';
 import VerificationInput from 'react-verification-input';
 import "../@styles/Verify.css"
 
 const Verify = () => {
 const [code, setCode]= useState();
-const [timer,  setTimer] = useState(50);
+const [timer,  setTimer] = useState(60);
 useEffect(() => {
 const timing = timer > 0 && setInterval(() => setTimer(timer - 1), 1000)
 
@@ -28,16 +29,11 @@ const timing = timer > 0 && setInterval(() => setTimer(timer - 1), 1000)
     <h6>Enter verification code</h6>
     <p>We have sent you a verification code, kindly enter the code below.</p>
       <div className='verification'>
-     <input type="tel"maxLength={1} />
-     <input type="tel" maxLength={1}/>
-     <input type="tel" maxLength={1}/>
-     <input type="tel" maxLength={1}/>
-     <input type="tel"maxLength={1} />
-     <input type="tel" maxLength={1}/>
+     <AuthCode length={4} allowedCharacters='numeric'/>
 
         </div>
         <div className='verify-block'>
-        <p>The code expires in <span>10:{timer}</span>  </p>
+        <p>The code expires in <span>00:{timer}</span>  </p>
         <button onClick={handlePofile} className='submit'> Submit</button>
         </div>
     </div>
